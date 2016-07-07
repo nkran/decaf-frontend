44f4ce9084bc10008226148591a2459123c2fe6c
Roland Andrei Groza <rolandjitsu@gmail.com>
2016-07-07 13:46:28 +0200

    chore: add jspm config

---
+++ jspm.config.js
@@ -0,0 +1,51 @@
+SystemJS.config({
+	production: false,
+	paths: {
+		"app/": "app/",
+		"github:": "jspm_packages/github/"
+	},
+	browserConfig: {
+		"baseURL": "/"
+	},
+	devConfig: {},
+	productionConfig: {},
+	packages: {
+		"app": {
+			"map": {
+				"./env": {
+					"~production": "./env.dev"
+				}
+			}
+		}
+	}
+});
+
+SystemJS.config({
+	packageConfigPaths: [
+		"github:*/*.json"
+	],
+	map: {
+		"angular": "github:angular/bower-angular@1.5.7",
+		"angular-material": "github:angular/bower-material@1.0.9"
+	},
+	packages: {
+		"github:angular/bower-material@1.0.9": {
+			"map": {
+				"css": "github:systemjs/plugin-css@0.1.23",
+				"angular-aria": "github:angular/bower-angular-aria@1.5.7",
+				"angular-animate": "github:angular/bower-angular-animate@1.5.7",
+				"angular": "github:angular/bower-angular@1.5.7"
+			}
+		},
+		"github:angular/bower-angular-aria@1.5.7": {
+			"map": {
+				"angular": "github:angular/bower-angular@1.5.7"
+			}
+		},
+		"github:angular/bower-angular-animate@1.5.7": {
+			"map": {
+				"angular": "github:angular/bower-angular@1.5.7"
+			}
+		}
+	}
+});
