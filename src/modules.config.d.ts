62ea21d169bc7a4a09b5a0a909cff7f7a09418fe
Roland Andrei Groza <rolandjitsu@gmail.com>
2016-07-11 14:11:37 +0200

    chore: turn modules config into typings

---
+++ src/modules.config.d.ts
@@ -0,0 +1,5 @@
+// Typing definitions for modules config.
+declare module 'src/modules.config' {
+	const MODULES_CONFIG: any[];
+	export default MODULES_CONFIG;
+}
--- src/modules.config.ts
+++
@@ -1,5 +0,0 @@
-// Intentionally left empty.
-// It serves as a placeholder for the actual modules config,
-// which is created during build.
-const MODULES_CONFIG = [];
-export default MODULES_CONFIG;
