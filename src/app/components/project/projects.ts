341656e1aae84e9100cbd78e5802855193fac073
Roland Andrei Groza <rolandjitsu@gmail.com>
2016-07-19 10:47:51 +0200

    refactor: simplify projects class

--- src/app/components/project/projects.ts
+++ src/app/components/project/projects.ts
@@ -2,16 +2,19 @@ import {module} from 'angular';
 
 
 const projects = module('platform.project._projects', []);
-class Projects<T> implements Iterator<T> {
+class Projects<T> implements Iterable<T> {
 	private _current: any = null;
 
-	private _pointer = 0;
 	private _items: any[] = [
 		{id: 0, name: 'Archimedes', color: '#5fc5ff'},
 		{id: 1, name: 'Umbrella', color: '#03d671'},
 		{id: 2, name: 'X', color: '#ff7f4d'},
 		{id: 3, name: 'Zeus', color: '#ff4747'}
 	];
+
+	[Symbol.iterator](): Iterator<T> {
+		return this._items.entries();
+	}
 
 	byId(id) {
 		return this.toArray().find((project) => (<any>project).id == id);
@@ -27,31 +30,7 @@ class Projects<T> implements Iterator<T> {
 	}
 
 	toArray(): T[] {
-		return Array.from(this);
-	}
-
-	// Iteration
-	// `for..of` and `.next()` iteration logic
-	get length() {
-		return this._items.length;
-	}
-
-	[Symbol.iterator](): IterableIterator<T> {
-		return this;
-	}
-	next(): IteratorResult<T> {
-		if (this._pointer < this._items.length) {
-			return {
-				done: false,
-				value: this._items[this._pointer++]
-			};
-		} else {
-			this._pointer = 0;
-			return {
-				done: true,
-				value: null
-			};
-		}
+		return this._items;
 	}
 }
 
