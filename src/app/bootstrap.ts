d041c38a69a32c374d006c12c024dc1318a109b0
Roland Andrei Groza <rolandjitsu@gmail.com>
2016-07-22 09:30:22 +0200

    docs: add some more notes about strict DI

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -7,6 +7,8 @@ import app from './app.component';
 
 angular.element(document).ready(() => {
 	angular.bootstrap(document.documentElement, [app.name], {
+		// Do not enable strict DI.
+		// NOTE: If we use `{strictDi: true}` we will not be able to use DI with ES6 classes
 		strictDi: false
 	});
 });
